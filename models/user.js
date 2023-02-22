1e30abfde2bc9fe7ddb1509bb9b3411dc9f613bf
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2023-02-22 11:45:11 +0200

    First saving

---
+++ models/user.js
@@ -0,0 +1,96 @@
+const mongoose = require('mongoose');
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const { JWT_SECRET } = process.env;
+
+const { REFRESH_TOKEN_SECRET } = process.env;
+
+
+const UserSchema = new mongoose.Schema({
+	email: {
+		type: String,
+		unique: true,
+		required: function () {
+			return !this.phoneNumber;
+		},
+		validate: {
+			validator: function (v) {
+				return /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/.test(v);
+			},
+			message: 'Please provide a valid email address.',
+		},
+	},
+	phoneNumber: {
+		type: String,
+		unique: false,
+		required: function () {
+			return !this.email;
+		},
+		validate: {
+			validator: function (v) {
+				return /^\+?\d{1,3}[- ]?\d{3}[- ]?\d{4}$/.test(v);
+			},
+			message: 'Please provide a valid phone number.',
+		},
+	},
+	password: {
+		type: String,
+		required: true,
+		minlength: 4,
+		maxlength: 16,
+	},
+	activated: {
+		type: Boolean,
+		default: false,
+	},
+	verificationCode: String,
+	createdAt: {
+		type: Date,
+		default: Date.now,
+	},
+	updatedAt: {
+		type: Date,
+		default: Date.now,
+	},
+	customerId: String,
+	jwttoken: String,
+	refreshtoken: String,
+	tokenDateEnd: Date,
+	roles: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'Role',
+		},
+	],
+});
+
+UserSchema.methods.generateAuthToken = function () {
+	const User = this;
+	const secret = JWT_SECRET;
+	const token = jwt.sign({ _id: User._id }, secret, {
+		expiresIn: '2m',
+	},);
+	User.token = token;
+}
+
+UserSchema.methods.generateRefreshToken = function () {
+	const User = this;
+	const secret = REFRESH_TOKEN_SECRET;
+	const refreshToken = jwt.sign({ _id: User._id }, secret, {
+		expiresIn: '5m',
+	},);
+	User.refreshToken = refreshToken;
+}
+
+UserSchema.pre('save', async function (next) {
+	const User = this;
+	if (User.isModified('password')) {
+		User.password = await bcryptjs.hash(User.password, 12);
+	}
+	next();
+});
+
+const User = mongoose.model('user', UserSchema);
+
+module.exports = User;
---
+++ routes/users.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
+const userController = require('../controllers/users.controller');
+
+const { auth } = require('../middleware/auth');
+
+
+router.post('/register', userController.newUser);
+
+router.get('/profile/:_id', auth, userController.getUser);
+
+router.get('/list', auth, userController.getUsers);
+
+router.put('/update/:_id', auth, userController.updateUser);
+
+router.delete('/delete/:_id', auth, userController.deleteUser);
+
+router.post('/login', userController.loginUser);
+
+router.post('/refresh', userController.refreshTokenUser);
+
+router.get('/logout', userController.logoutUser);
+
+module.exports = router;
